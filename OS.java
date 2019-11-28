08a59d770c62002b71ced06618a48819c83edab0
Aidan McPeake <46015459+amcpeake@users.noreply.github.com>
2019-11-28 09:03:26 -0500

    Replaced attributes with getters

--- OS.java
+++ OS.java
@@ -17,23 +17,26 @@ import java.util.stream.Collectors;
 public class OS {
 	private static CommandController controller = CommandController.getInstance();
 	
-	protected final String name; // Mapped device name
-    protected final String path; // Path to mapped device (/dev/mapper/<Device Name>)
-    
-    private OS(String name)  {
-        this.name = name;
-        this.path = "/dev/mapper/" + name;
-    }
-    
-    protected static OS get(String input) throws InterruptedException, IOException, APIAccessException {
-    	return new OS(findOS(input).stream() // Get first result given input string, or throw exception if none found
-    			.findFirst()
-    			.orElseThrow(() -> new APIAccessException(input + " is not a valid OS name", 400)));
-    }
-    
-    public static List<String> findOS(String keyword) throws InterruptedException, IOException { // Get a list of OS names which match a given keyword
-        return Arrays.asList(controller.command("ls /dev/mapper").getOutput().split("\n")).stream()
-        		.filter(image -> Objects.equals(keyword,  "") || image.toUpperCase().contains(keyword.toUpperCase()))
-        		.collect(Collectors.toList());
-    }
+    	private final String name; // Mapped device name
+	private final String path; // Path to mapped device (/dev/mapper/<Device Name>)
+
+	private OS(String name)  {
+		this.name = name;
+		this.path = "/dev/mapper/" + name;
+	}
+
+	protected String getName() { return this.name; }
+	protected String getPath() { return this.path; }
+
+	protected static OS get(String input) throws InterruptedException, IOException, APIAccessException {
+		return new OS(findOS(input).stream() // Get first result given input string, or throw exception if none found
+				.findFirst()
+				.orElseThrow(() -> new APIAccessException(input + " is not a valid OS name", 400)));
+	}
+
+	public static List<String> findOS(String keyword) throws InterruptedException, IOException { // Get a list of OS names which match a given keyword
+		return Arrays.asList(controller.command("ls /dev/mapper").getOutput().split("\n")).stream()
+				.filter(image -> Objects.equals(keyword,  "") || image.toUpperCase().contains(keyword.toUpperCase()))
+				.collect(Collectors.toList());
+	}
 }
